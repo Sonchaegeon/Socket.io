a465be0b8ca06eafe298dc59a78e72c73d6d2d59
son <thscorjs1002@naver.com>
2020-08-23 17:44:55 +0900

    Socket.io Connection chat.html

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+var path = require('path');
+var http = require('http');
 var express = require('express');
+var socketio = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+const io = socketio(server);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+// static 폴더 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 
+io.on('connection', socket => { 
+    console.log('New WS Connection...');
+});
+
+server.listen(3000, () => console.log('Server running on port 3000'));
