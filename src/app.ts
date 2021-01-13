618a1a3a35dd88980c1cc24c7cb6f52ce8ff1321
sonchaegeon <thscorjs1002@naver.com>
2021-01-13 19:16:26 +0900

    UPDATE; Error handle

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+
 dotenv.config();
+import { HttpError } from "./types/HttpError";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Setup Error Handlers
-import * as errorHandlers from './handlers/errorHandlers';
-app.use(errorHandlers.notFound);
-app.use(errorHandlers.mongoseErrors);
-if(process.env.ENV === 'DEVELOPMENT'){
-    app.use(errorHandlers.developmentErrors);
-} else {
-    app.use(errorHandlers.productionErrors);
-}
+
+// Error Handle
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const err: Error = new HttpError(404, `${req.method}, ${req.url} 라우터가 없습니다.`);
+    next(err);
+  });
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status).json({
+        message: err.message,
+        error: process.env.ENV !== 'PRODUCTION' ? err : {},
+    })
+})
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
