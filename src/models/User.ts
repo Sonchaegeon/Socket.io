e9d6309b9fd7f839f70b3f35a82fabdf215f1944
sonchaegeon <thscorjs1002@naver.com>
2021-01-14 21:51:31 +0900

    CREATE; user model

---
+++ src/models/User.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'Name is required!'
+    },
+    email: {
+        type: String,
+        required: "Email is required!"
+    },
+    password: {
+        type: String,
+        required: "Password is required!"
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
