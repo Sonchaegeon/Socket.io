d5d756979279e56564fa604759d6070f0ac1415c
sonchaegeon <thscorjs1002@naver.com>
2021-01-14 23:43:08 +0900

    UPDATE; login, register router

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 
+import * as indexController from "../controllers/userController";
 import { BusinessLogic } from "../types/BusinessLogic";
 import { errorHandler } from "../middlewares/errorHandler";
 
 const router: Router = Router();
+
+const loginHandler: BusinessLogic = errorHandler(indexController.login);
+const registerHandler: BusinessLogic = errorHandler(indexController.register);
+
+router.post('/login', loginHandler);
+router.post('/register', registerHandler);
 
 export default router;
