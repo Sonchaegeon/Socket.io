30a6f90a0a3e08d69e1959ce80ecfa594b30f521
sonchaegeon <thscorjs1002@naver.com>
2021-01-14 21:49:13 +0900

    CREATE; chatroom model

---
+++ src/models/Chatroom.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const chatroomSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'Name is required!'
+    },
+});
+
+module.exports = mongoose.model('Chatroom', chatroomSchema);
