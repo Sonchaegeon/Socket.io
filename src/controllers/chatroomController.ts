f98c802e18e266a719a308db8bbe573a55b1441e
sonchaegeon <thscorjs1002@naver.com>
2021-01-15 02:34:57 +0900

    CREATE; create chatroom api

---
+++ src/controllers/chatroomController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { HttpError } from "../types/HttpError";
+
+const Chatroom = mongoose.model("Chatroom");
+
+export const createChatroom = async (req: Request, res: Response) => {
+    const { name } = req.body;
+
+    const nameRegex = /^["A-Za-z\s"]+$/;
+    if(!nameRegex.test(name)) throw new HttpError(400, "Chatroom name can contain only alphabets");
+
+    const chatroomExists = await Chatroom.findOne({ name });
+    if(chatroomExists) throw new HttpError(400, "Chatroom with that name already exists");
+
+    const chatroom = new Chatroom({ name });
+
+    await chatroom.save();
+
+    return res.json({
+        message: "Chatroom created!",
+    });
+}
