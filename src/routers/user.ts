0061cb3db794b3b26b18d2987c6b3e1799afe478
sonchaegeon <thscorjs1002@naver.com>
2021-01-14 22:53:45 +0900

    CREATE; user router

---
+++ src/routers/user.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+import { BusinessLogic } from "../types/BusinessLogic";
+import { errorHandler } from "../middlewares/errorHandler";
+
+const router: Router = Router();
+
+export default router;
