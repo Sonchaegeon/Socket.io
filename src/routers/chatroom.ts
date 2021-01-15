780b1843aa649928613b094d9ffb5e3e361ecbbd
sonchaegeon <thscorjs1002@naver.com>
2021-01-15 20:42:27 +0900

    UPDATE; get all chatroom router

--- src/routers/chatroom.ts
+++ src/routers/chatroom.ts
@@ -8,7 +8,9 @@ import tokenValidation from '../middlewares/verifyToken';
 const router: Router = Router();
 
 const createChatroomHandler: BusinessLogic = errorHandler(chatroomController.createChatroom);
+const getAllChatroomHandler: BusinessLogic = errorHandler(chatroomController.getAllChatroom);
 
 router.post('/', tokenValidation, createChatroomHandler);
+router.get('/', tokenValidation, getAllChatroomHandler);
 
 export default router;
